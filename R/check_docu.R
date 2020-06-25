3843d8ddbb35d746ca5abde8bb6e329dfb05684b
Ben <benj.becker@web.de>
2020-06-25 12:53:39 +0200

    0.2.0
    check_docu for multiple savs and pdfs

--- R/check_docu.R
+++ R/check_docu.R
@@ -1,47 +1,77 @@
 ####
 #############################################################################
-#' Check documentation of a data set.
+#' Check documentation of data sets.
 #'
-#' Check if all variables of a data set saved as \code{.sav} are included in the \code{.pdf} documentation.
+#' Check if all variables of one or multiple data sets saved as \code{sav} are included in the \code{pdf} documentation.
 #'
-#' tbd
+#'A common requirement for data documentation is a complete codebook. The \code{check_docu} function can be used to check, whether all variables (column names) in one or multiple \code{sav} data sets are mentioned in the documentation, provided as \code{pdf} files. Multiple \code{pdf} files are treated as one single \code{pdf} file. For multiple data sets the output is sorted by data set and the \code{data_set} column indicates to which data set the variable name belongs.
 #'
-#'@param sav_path Path to the \code{.sav} file.
-#'@param pdf_path Path to the \code{.pdf} file.
+#'For easier reading of the output, the output can be written to an excel file, for example using the \code{\link[openxlsx]{write.xlsx}} function from the \code{openxlsx} package or the \code{\link[eatAnalysis]{write_xlsx}} function from the \code{eatAnalysis} package.
+#'
+#'@param sav_path Character vector with paths to the \code{.sav} files.
+#'@param pdf_path Character vector with paths to the \code{.pdf} files.
 #'@param post_words Number of words after the variable names that should be extracted from the PDF.
 #'
-#'@return A \code{data.frame} with the variable names, count of mentions in the PDF and words after the variable names.
+#'@return A \code{data.frame} with the variable names, count of mentions in the \code{pdf} (\code{count}), words after the variable names (\code{post}) and the name of the data set in which the variable occurs (\code{data_set}).
 #'
 #'@examples
-#' # tbd
+#' # File pathes
+#' sav_path1 <- system.file("extdata", "helper_spss_p1.sav", package = "eatFDZ")
+#' sav_path2 <- system.file("extdata", "helper_spss_p2.sav", package = "eatFDZ")
+#' pdf_path1 <- system.file("extdata", "helper_codebook_p1.pdf", package = "eatFDZ")
+#' pdf_path2 <- system.file("extdata", "helper_codebook_p2.pdf", package = "eatFDZ")
+#'
+#' check_df <- check_docu(sav_path = c(sav_path1, sav_path2),
+#'                        pdf_path = c(pdf_path1, pdf_path2), post_words = 2)
 #'
 #'@export
 check_docu <- function(sav_path, pdf_path, post_words = 2) {
-  if(!is.character(sav_path) || length(sav_path) != 1) stop("sav_path needs to be a single path to a .sav file.")
-  if(!is.character(pdf_path) || length(pdf_path) != 1) stop("pdf_path needs to be a single path to a .pdf file.")
+  if(!is.character(sav_path) || length(sav_path) < 1) stop("sav_path needs to be at least one path to a .sav file.")
+  if(!is.character(pdf_path) || length(pdf_path) < 1) stop("pdf_path needs to be at least one path to a .pdf file.")
 
-  gads <- suppressWarnings(eatGADS::import_spss(sav_path, checkVarNames = FALSE))
-  nams <- eatGADS::namesGADS(gads)
-  names(nams) <- nams
+  out_list <- lapply(sav_path, function(single_sav_path) {
+    gads <- suppressWarnings(eatGADS::import_spss(single_sav_path, checkVarNames = FALSE))
+    nams <- eatGADS::namesGADS(gads)
+    names(nams) <- nams
 
-  docu <- readtext::readtext(pdf_path)
-  # to corpus
-  corp_docu <- quanteda::corpus(docu)
-  summary(corp_docu)
-  # to token
-  tok_docu <- quanteda::tokens(corp_docu)
-  # test
-  test <- lapply(nams, function(nam) {
-    out <- quanteda::kwic(tok_docu, pattern = nam, window = post_words)
-    post <- out$post
-    if(nrow(out) == 0) post <- NA
-    data.frame(variable = nam,
-               count = nrow(out),
-               post = post, stringsAsFactors = FALSE)
+    docu <- readtext::readtext(pdf_path)
+    # to corpus
+    corp_docu <- quanteda::corpus(docu)
+    summary(corp_docu)
+    # to token
+    tok_docu <- quanteda::tokens(corp_docu)
+    # test
+    test <- lapply(nams, function(nam) {
+      out <- quanteda::kwic(tok_docu, pattern = nam, window = post_words)
+      post <- out$post
+      if(nrow(out) == 0) post <- NA
+      data.frame(variable = nam,
+                 count = nrow(out),
+                 post = post, stringsAsFactors = FALSE)
+    })
+    do.call(rbind, test)
   })
-  do.call(rbind, test)
+  #if(length(sav_path) > 1) browser()
+  do_call_rbind_withName(out_list, name = basename(sav_path), colName = "data_set")
 }
 
+
+do_call_rbind_withName <- function (label_list, name = names(label_list), colName){
+  label_list <- Map(function(df, name) {
+    if (is.null(df)) return(df)
+    data.frame(df, placeHolder = rep(name, nrow(df)), stringsAsFactors = FALSE)
+  }, df = label_list, name = name)
+  label_df <- do.call(rbind, label_list)
+  rownames(label_df) <- NULL
+  names(label_df)[ncol(label_df)] <- colName
+
+  label_df
+}
+
+
+
+### open to do: move do_call_rbind_withName to eatGADS (replace inside mergeLabels)
+# import here
 
 
 
--- tests/testthat/test-check_docu.R
+++ tests/testthat/test-check_docu.R
@@ -1,14 +1,72 @@
 
 
-#out <- check_docu(sav_path = "C:/Benjamin_Becker/02_Repositories/packages/eatFDZ/tests/testthat/helper_spss.sav", pdf_path = "C:/Benjamin_Becker/02_Repositories/packages/eatFDZ/tests/testthat/helper_codebook.pdf")
+#out <- check_docu(sav_path = "tests/testthat/helper_spss.sav", pdf_path = "tests/testthat/helper_codebook.pdf")
 
-test_that("multiplication works", {
+test_that("One pdf, one .sav data set", {
   out <- check_docu(sav_path = "helper_spss.sav",
              pdf_path = "helper_codebook.pdf")
-  expect_equal(names(out), c("variable", "count", "post"))
+  expect_equal(names(out), c("variable", "count", "post", "data_set"))
   expect_equal(out[out$variable == "ID", "count"], 1)
   expect_equal(out[2, "count"], 2)
   expect_equal(out[out$variable == "Var3", "count"], 0)
 
   expect_equal(out[out$variable == "ID", "post"], "ID-Variable Var1")
+  expect_equal(out$data_set[1], "helper_spss.sav")
+  expect_equal(length(unique(out$data_set)), 1)
 })
+
+
+### write separate data.frames as .sav for testing
+#df1 <- data.frame(Var2 = 1, ID = 1)
+#haven::write_sav(df1, "tests/testthat/helper_spss_p1.sav")
+#df2 <- data.frame(Var1 = 1, ID = 1, Var3 = 4)
+#haven::write_sav(df2, "tests/testthat/helper_spss_p2.sav")
+#haven::read_sav("tests/testthat/helper_spss_p2.sav")
+
+
+#out <- check_docu(sav_path = c("tests/testthat/helper_spss_p1.sav", "tests/testthat/helper_spss_p2.sav"), pdf_path = "tests/testthat/helper_codebook.pdf")
+
+test_that("One pdf, mulitple .sav data set", {
+  out <- check_docu(sav_path = c("helper_spss_p1.sav", "helper_spss_p2.sav"),
+                    pdf_path = "helper_codebook.pdf")
+  expect_equal(names(out), c("variable", "count", "post", "data_set"))
+  expect_equal(out[out$variable == "ID", "count"], c(1, 1))
+  expect_equal(out[3, "count"], 2)
+  expect_equal(out[out$variable == "Var3", "count"], 0)
+
+  expect_equal(out[out$variable == "ID", "post"][1], "ID-Variable Var1")
+  expect_equal(out$data_set[1], "helper_spss_p1.sav")
+  expect_equal(out$data_set[3], "helper_spss_p2.sav")
+  expect_equal(out$post[6], NA_character_)
+  expect_equal(out$count[6], 0)
+})
+
+#out <- check_docu(sav_path = c("tests/testthat/helper_spss.sav"), pdf_path = c("tests/testthat/helper_codebook_p1.pdf", "tests/testthat/helper_codebook_p2.pdf"))
+
+test_that("Multiple pdf, one .sav data set", {
+  out <- check_docu(sav_path = c("helper_spss.sav"),
+                    pdf_path = c("helper_codebook_p1.pdf", "helper_codebook_p2.pdf"))
+  expect_equal(names(out), c("variable", "count", "post", "data_set"))
+  expect_equal(out[out$variable == "ID", "count"], 1)
+  expect_equal(out[2, "count"], 3)
+  expect_equal(out[out$variable == "Var3", "count"], 0)
+
+  expect_equal(out[out$variable == "ID", "post"], "ID-Variable Var1")
+  expect_equal(out$data_set[1], "helper_spss.sav")
+  expect_equal(length(unique(out$data_set)), 1)
+})
+
+
+
+test_that("Rbind with name column", {
+  l <- lapply(1:3, function(x) data.frame(v1 = x))
+  out <- do_call_rbind_withName(l, name = c("one", "two", "three"), colName = "df")
+
+  expect_equal(out$v1, 1:3)
+  expect_equal(out$df, c("one", "two", "three"))
+})
+
+
+
+
+
