371b15e3310cd71a6ae04b07132c667a7e849ae2
ruckeran <annegret.rucker@hu-berlin.de>
2026-01-22 15:43:13 +0100

    first try for function docu

--- R/check_var_names_length.R
+++ R/check_var_names_length.R
@@ -1,16 +1,24 @@
 #' Check for lengthy variable names.
 #'
-#' Check for lengthy variable names of variables in a \code{GADSdat}.
-#'
+#' This function checks whether variable names in a \code{GADSdat} object exceed the allowed number of characters.
+#' This can help ensure that variable names adhere to naming conventions and avoid potential issues with downstream processing or compatibility.
 #'
 #'@param GADSdat \code{GADSdat} object.
-#'@param boundary Numeric vector of lenght 1. Boundary: Number of characters variables names are allowed
-#'to have.
+#'@param boundary An integer specifying the maximum length (number of characters) a variable name is allowed to have. Default is 30.
 #'
-#'@return Returns the test report.
+#'@return A \code{data.frame} containing the variable names that exceed the boundary, along with their respective character counts.
+#' If all variable names are within the boundary, an empty \code{data.frame} is returned.
 #'
 #'@examples
-#'# tbd
+#' # Example usage:
+#' # Load example GADSdat object
+#' GADSdat <- eatGADS::import_spss(system.file("extdata", "example_data2.sav", package = "eatFDZ"))
+#'
+#' # Check for variable names longer than 30 characters
+#' lengthy_names <- check_var_names_length(GADSdat, boundary = 30)
+#'
+#' # Print the result
+#' print(lengthy_names)
 #'
 #'@export
 check_var_names_length <- function(GADSdat, boundary = 30) {
