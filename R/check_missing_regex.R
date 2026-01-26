66561a5e134f1feb4b956ff330fd40574c0191e4
ruckeran <annegret.rucker@hu-berlin.de>
2026-01-26 12:00:34 +0100

    updated docu for check_missing_regex()

--- R/check_missing_regex.R
+++ R/check_missing_regex.R
@@ -1,19 +1,38 @@
-#' Check missing range.
+#' Check if labeled values match a missing value regex pattern
 #'
-#' Check all labeled values if they fall in the range for missing values \code{GADSdat}.
+#' This function checks whether labeled values in the meta data of a \code{GADSdat} object match a specified regular expression (\code{missingRegex})
+#' for missing value labels. It identifies values that are marked as valid but whose labels suggest they should be declared as missing,
+#' based on the provided pattern.
 #'
-#' Checks include
+#' @param GADSdat A \code{GADSdat} object containing the data to be checked.
+#' @param missingRegex A character string specifying the regular expression pattern used to identify value labels that should be treated as missing.
+#' The default pattern is \code{"missing|omitted|not reached|nicht beantwortet|ausgelassen"}.
+#'
+#' @return A \code{data.frame} listing value labels that match the specified \code{missingRegex} pattern but are marked as valid in the meta data.
+#' The output includes the following columns:
 #' \itemize{
-#' \item ...
+#'   \item \code{varName}: The name of the variable containing the value.
+#'   \item \code{value}: The value itself that has a label matching the \code{missingRegex}.
+#'   \item \code{valLabel}: The label associated with the value.
+#'   \item \code{missings}: The current missing status in the meta data (should be \code{"valid"} if reported here).
 #' }
+#' If no issues are found, the function returns an empty \code{data.frame}.
 #'
-#'@param GADSdat A \code{GADSdat} object.
-#'@param missingRegex Value labels which should be declared as missings.
+#' @examples
+#' # Example usage:
 #'
-#'@return Returns the test report.
+#' # Load example GADSdat object
+#' GADSdat <- eatGADS::import_spss(system.file("extdata", "example_data2.sav", package = "eatFDZ"))
 #'
-#'@examples
-#'# tbd
+#' # Check if labeled values match the missing value regex pattern
+#' missing_regex_report <- check_missing_regex(GADSdat)
+#'
+#' # Print the report
+#' print(missing_regex_report)
+#'
+#' # Using a custom regex pattern
+#' custom_report <- check_missing_regex(GADSdat, missingRegex = "unanswered|unknown|keine Angabe")
+#' print(custom_report)
 #'
 #'@export
 check_missing_regex <- function(GADSdat, missingRegex = "missing|omitted|not reached|nicht beantwortet|ausgelassen") {
