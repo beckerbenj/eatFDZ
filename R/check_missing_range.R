dd55646a0b013bdab57167618caa89fd073628f6
ruckeran <annegret.rucker@hu-berlin.de>
2026-01-26 11:57:33 +0100

    updated docu of check_missing_range()

--- R/check_missing_range.R
+++ R/check_missing_range.R
@@ -1,19 +1,35 @@
-#' Check missing range.
+#' Check if labeled values fall within the specified missing value range
 #'
-#' Check all labeled values if they fall in the range for missing values \code{GADSdat}.
+#' This function checks whether all labeled values in the meta data of a \code{GADSdat} object fall within the specified range for missing values.
+#' It identifies values that are labeled as valid but should be declared as missing based on the defined \code{missingRange}.
 #'
-#' Checks include
+#' @param GADSdat A \code{GADSdat} object containing the data to be checked.
+#' @param missingRange A numeric vector specifying the range of values that should be treated as missing.
+#' The default is \code{-50:-99}, but this can be customized.
+#'
+#' @return A \code{data.frame} listing labeled values that are within the specified \code{missingRange} but marked as valid in the meta data.
+#' The output includes the following columns:
 #' \itemize{
-#' \item ...
+#'   \item \code{varName}: The name of the variable containing the value.
+#'   \item \code{value}: The value itself that falls within the \code{missingRange}.
+#'   \item \code{valLabel}: The label associated with the value.
+#'   \item \code{missings}: The current missing status in the meta data (should be \code{"valid"} if reported here).
 #' }
+#' If all values within the \code{missingRange} are already declared as missing, the function returns an empty \code{data.frame}.
 #'
-#'@param GADSdat \code{GADSdat} object.
-#'@param missingRange Values which should be declared as missings.
+#' @examples
+#' # Example usage:
 #'
-#'@return Returns the test report.
+#' # Load example GADSdat object
+#' GADSdat <- eatGADS::import_spss(system.file("extdata", "example_data2.sav", package = "eatFDZ"))
 #'
-#'@examples
-#'# tbd
+#' # Check if labeled values are within the missing value range (-50 to -99 by default)
+#' missing_range_report <- check_missing_range(GADSdat)
+#'
+#' # Print the report
+#' print(missing_range_report)
+#'
+#' @export
 #'
 #'@export
 check_missing_range <- function(GADSdat, missingRange = -50:-99) {
