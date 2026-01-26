3a8e4e31a399815945f0b4b8067d3334dfb51934
ruckeran <annegret.rucker@hu-berlin.de>
2026-01-26 15:44:54 +0100

    updated docu for check_file_name()

--- R/check_file_name.R
+++ R/check_file_name.R
@@ -1,19 +1,22 @@
-#' Check file name.
+#' Check and validate a file name
 #'
-#' Check and validate a file name.
+#' This function checks the validity of a file name, ensuring that it does not contain special characters (e.g., \code{Umlaute})
+#' or spaces. File names with problematic encoding or formatting are flagged, as these issues may cause errors when processing
+#' files in different systems or environments.
 #'
-#' Checks in variable names include
-#' \itemize{
-#' \item special signs such as \code{Umlaute}
-#' \item spaces
-#' }
+#' @param path A character string specifying the file path to be checked.
 #'
-#'@param path File path.
+#' @return This function returns an error if any issues are detected with the file name (e.g., special characters or spaces).
+#' If the file name is valid, no output is returned.
 #'
-#'@return Returns the test report.
+#' @examples
+#' # Example of a valid file name
+#' valid_path <- system.file("extdata", "example_data.sav", package = "eatFDZ")
+#' check_file_name(valid_path)
 #'
-#'@examples
-#'# tbd
+#' # Example of an invalid file name (this will throw an error)
+#' # invalid_path <- "invalid file name.sav"
+#' # check_file_name(invalid_path)
 #'
 #'@export
 check_file_name <- function(path) {
