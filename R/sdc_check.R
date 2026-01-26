b17a31c9fd81cc71df20bdb530c687ef505058c9
ruckeran <annegret.rucker@hu-berlin.de>
2026-01-26 16:26:08 +0100

    updated docu for sdc_check(), reverted docu for check_sdc()

--- R/sdc_check.R
+++ R/sdc_check.R
@@ -4,33 +4,46 @@
 
 #### Perform FDZ Data Cleansing
 #############################################################################
-#' Create a Statistical Disclosure Control Report.
+#' Create a Statistical Disclosure Control Report
 #'
-#' Create a statistical disclosure control report: Which variables have categories with low absolute frequencies,
-#' which might lead to statistical data disclosure issues?
+#' This function generates a statistical disclosure control (SDC) report, identifying variables with categories
+#' that have low absolute frequencies. Such low-frequency categories could potentially lead to statistical
+#' data disclosure issues, particularly in datasets involving individual-level data from studies like
+#' large-scale assessments. The function currently performs only a uni-variate check, flagging categories
+#' with a frequency below a specified threshold.
 #'
-#' Individual participants of studies such as educational large-scale assessments usually must remain non-identifiable on individual level.
-#' This function checks all variables in a \code{GADSdat} object
-#' for low frequency categories which might lead to statistical disclosure control issues.
-#' Currently, only a uni-variate check is implemented.
+#' @param fileName A character string specifying the path to the SPSS file to import as a \code{GADSdat} object.
+#' @param boundary An integer specifying the frequency threshold for identifying low-frequency categories.
+#' Categories with less than or equal to this number of observations will be flagged. The default value is \code{5}.
+#' @param exclude An optional character vector containing variable names that should be excluded from the report.
+#' @param encoding An optional character string specifying the character encoding for importing the SPSS file.
+#' If \code{NULL} (default), the encoding specified in the file is used.
 #'
-#'@param fileName Character string of the SPSS file
-#'@param boundary Integer number: categories with less than or equal to \code{boundary} observations will be flagged
-#'@param exclude Optional: character vector of variable names which should be excluded from the report
-#'@param encoding Optional: The character encoding used for reading the \code{.sav} file. The default, \code{NULL}, uses the encoding specified in the file, but sometimes this value is incorrect and it is useful to be able to override it.
+#' @return A \code{data.frame} summarizing categories with low frequencies, including the following columns:
+#' \itemize{
+#'   \item \code{variable}: The name of the variable with low-frequency categories.
+#'   \item \code{varLab}: The label for the variable (if present).
+#'   \item \code{existVarLab}: Whether a variable label exists (\code{TRUE} or \code{FALSE}).
+#'   \item \code{existValLab}: Whether value labels exist for the variable (\code{TRUE} or \code{FALSE}).
+#'   \item \code{skala}: Information on the variable type/classification.
+#'   \item \code{nKatOhneMissings}: The total number of non-missing categories.
+#'   \item \code{nValid}: The total number of valid observations for the variable.
+#'   \item \code{nKl5}: Indicator for variables with categories flagged as low frequency (\code{TRUE} or \code{FALSE}).
+#'   \item \code{exclude}: Whether the variable has been excluded based on the \code{exclude} argument.
+#' }
 #'
-#'@return A \code{data.frame}.
+#' @examples
+#' # Load an example SPSS file
+#' sav_path <- system.file("extdata", "LV_2011_CF.sav", package = "eatFDZ")
 #'
-#'@examples
-#'sav_path <- system.file("extdata", "LV_2011_CF.sav", package = "eatFDZ")
+#' # Exclude unique identifier variables from the SDC check
+#' exclude_vars <- c("idstud_FDZ", "idsch_FDZ")
 #'
-#'## don't report low frequencies for unique id variables
-#'exclude<- c("idstud_FDZ", "idsch_FDZ")
+#' # Generate the SDC report
+#' sdc_report <- sdc_check(fileName = sav_path, boundary = 5, exclude = exclude_vars)
 #'
-#'##
-#'sdc_report <- sdc_check(fileName = sav_path, exclude=exclude)
-#'
-#'
+#' # Print the SDC report
+#' print(sdc_report)
 #'
 #'@export
 sdc_check <- function ( fileName, boundary = 5, exclude = NULL, encoding = NULL) {
